7551515776d02c08ecb3e0a80b9e2e5a23fe4084
Derkune <derkunebandcamp@gmail.com>
2025-11-14 17:31:49 +0200

    progress on implementing the language

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from typing import Any, Union, Tuple, Dict, Tuple, List, Callable, Optional, Generator
+from typing import (
+    Any,
+    Union,
+    Tuple,
+    Dict,
+    Tuple,
+    List,
+    Callable,
+    Optional,
+    Generator,
+    Final,
+)
 from dataclasses import dataclass
 
 
@@ -79,18 +90,31 @@ def Lang_nesting(val: LangType) -> LangType:
 Position = Tuple[int, int]
 
 
-class SpecialPosition:
-    pass
-
-
-ENTRY_POINT: SpecialPosition = SpecialPosition()
-
-
 @dataclass
+class SpecialPosition:
+    hash_arg: str
+
+    def __hash__(self) -> int:
+        return hash(self.hash_arg)
+
+    def __eq__(self, value: object) -> bool:
+        return isinstance(value, SpecialPosition) and value.hash_arg == self.hash_arg
+
+
+GeneralizedPosition = Union[Position, SpecialPosition]
+
+
+ENTRY_POINT: SpecialPosition = SpecialPosition("ENTRY_POINT")
+ARG0: SpecialPosition = SpecialPosition("ARG0")
+ARG1: SpecialPosition = SpecialPosition("ARG1")
+ARG2: SpecialPosition = SpecialPosition("ARG2")
+
+
+@dataclass(kw_only=True)
 class Function:
     symbol_mapping: Dict[Position, str]
     """Mapping from positions in the function to symbols at those positions"""
-    input_mapping: Dict[Union[SpecialPosition, Position], List[Position]]
+    input_mapping: Dict[GeneralizedPosition, List[GeneralizedPosition]]
     """Mapping from positions of function calls to tuples of function calls on which they depend"""
     input_positions: List[SpecialPosition]
     """List of places where the argument values will be placed in FunctionFrame on evaluation"""
@@ -99,55 +123,114 @@ class Function:
 @dataclass
 class FunctionFrame:
     function_reference: Function
-    _arguments: List
+    _arguments: List[LangType]
 
     def __post_init__(self) -> None:
-        self.arguments: Dict[SpecialPosition, str] = {
+        self.arguments: Dict[SpecialPosition, LangType] = {
             pos: arg
             for pos, arg in zip(
                 self.function_reference.input_positions, self._arguments, strict=True
             )
         }
 
-    def get_symbol_for_pos(self, pos: Union[SpecialPosition, Position]) -> str:
+    def get_symbol_for_pos(
+        self, pos: GeneralizedPosition
+    ) -> Tuple[Union[str, LangType], GeneralizedPosition]:
         """
         Return either the symbol representing a constant placed in place of an argument
         or a symbol in the body of the function.
         """
         if isinstance(pos, SpecialPosition):
-            return self.arguments[pos]
+            if pos is ENTRY_POINT:
+                entry_pos = self.function_reference.input_mapping[ENTRY_POINT]
+                assert (
+                    len(entry_pos) == 1
+                ), "input_mapping to ENTRY_POINT should contain 1 Position"
+                assert isinstance(
+                    entry_pos[0], tuple
+                ), "input_mapping to ENTRY_POINT should contain 1 Position"
+                symbol_on_pos = self.function_reference.symbol_mapping[entry_pos[0]]
+                return symbol_on_pos, entry_pos[0]
 
-        return self.function_reference.symbol_mapping[pos]
+            return self.arguments[pos], pos
 
-
-class Constant:
-    pass
+        return self.function_reference.symbol_mapping[pos], pos
 
 
 class Operation:
-    def __call__(self, *args: Any, **kwds: Any) -> Constant:
+    def __call__(self, *args: Any, **kwds: Any) -> LangType:
         """Placeholder"""
         ...
+
+    def __hash__(self) -> int: ...
+
+    def __eq__(self, value: object) -> bool:
+        return type(value) is type(self)
+
+
+class OperationCompare(Operation):
+    def __call__(self, *args: Any, **kwds: Any) -> LangType:
+        return Lang_compare(*args)
+
+    def __hash__(self) -> int:
+        return hash(">")
+
+
+class OperationPlus(Operation):
+    def __call__(self, *args: Any, **kwds: Any) -> LangType:
+        return Lang_plus(*args)
+
+    def __hash__(self) -> int:
+        return hash("+")
+
+
+class OperationMinus(Operation):
+    def __call__(self, *args: Any, **kwds: Any) -> LangType:
+        return Lang_minus(*args)
+
+    def __hash__(self) -> int:
+        return hash("-")
+
+
+class OperationNest(Operation):
+    def __call__(self, *args: Any, **kwds: Any) -> LangType:
+        return Lang_nesting(*args)
+
+    def __hash__(self) -> int:
+        return hash("~")
 
 
 class ChoosePrimitive:
     """Evaluates the 0th argument, and chooses 1st or 2nd to evaluate and return"""
 
-    def __call__(self, *args: Any, **kwds: Any) -> Position:
-        """Placeholder"""
-        ...
+    def __call__(
+        self,
+        condition: LangType,
+        pos1: GeneralizedPosition,
+        pos2: GeneralizedPosition,
+    ) -> GeneralizedPosition:
+        if Lang_choice_comparison(condition):
+            return pos1
+        else:
+            return pos2
+
+    def __hash__(self) -> int:
+        return hash("?")
+
+    def __eq__(self, value: object) -> bool:
+        return type(value) is type(self)
 
 
-PossibleFunc = Union[Operation, Constant, Function, ChoosePrimitive]
+PossibleFunc = Union[Operation, LangType, Function, ChoosePrimitive]
 
 
-ExecuteGenReturnType = Generator[str, None, Constant]
+ExecuteGenReturnType = Generator[str, None, LangType]
 
 
 def _handle_choose_gen(
     current_func: FunctionFrame,
     choose_op: ChoosePrimitive,
-    input_positions: List[Position],
+    input_positions: List[GeneralizedPosition],
     function_mapping: Dict[str, PossibleFunc],
 ) -> ExecuteGenReturnType:
     """Handles the lazy evaluation of the ChoosePrimitive."""
@@ -161,11 +244,11 @@ def _handle_choose_gen(
 
 def _resolve_all_inputs_gen(
     current_func: FunctionFrame,
-    input_positions: List[Position],
+    input_positions: List[GeneralizedPosition],
     function_mapping: Dict[str, PossibleFunc],
-) -> Generator[str, None, List[Constant]]:
+) -> Generator[str, None, List[LangType]]:
     """Recursively executes all inputs and returns the resulting constants."""
-    ret_val: List[Constant] = []
+    ret_val: List[LangType] = []
     for input_pos in input_positions:
         item_val = yield from _execute_gen(current_func, input_pos, function_mapping)
         ret_val.append(item_val)
@@ -176,7 +259,7 @@ def _resolve_all_inputs_gen(
 def _handle_operation_gen(
     current_func: FunctionFrame,
     operation: Operation,
-    input_positions: List[Position],
+    input_positions: List[GeneralizedPosition],
     function_mapping: Dict[str, PossibleFunc],
 ) -> ExecuteGenReturnType:
     """Resolves all inputs and applies the operation to them."""
@@ -189,7 +272,7 @@ def _handle_operation_gen(
 def _handle_user_function_gen(
     current_func: FunctionFrame,
     user_function: Function,
-    input_positions: List[Position],
+    input_positions: List[GeneralizedPosition],
     function_mapping: Dict[str, PossibleFunc],
 ) -> ExecuteGenReturnType:
     """Resolves all inputs and executes a user-defined function in a new frame."""
@@ -203,13 +286,17 @@ def _handle_user_function_gen(
 
 def _execute_gen(
     current_func: FunctionFrame,
-    pos: Union[SpecialPosition, Position],
+    pos: GeneralizedPosition,
     function_mapping: Dict[str, PossibleFunc],
 ) -> ExecuteGenReturnType:
-    symbol_on_pos = current_func.get_symbol_for_pos(pos)
-    function_on_pos = function_mapping[symbol_on_pos]
+    symbol_or_const_on_pos, pos = current_func.get_symbol_for_pos(pos)
 
-    if isinstance(function_on_pos, Constant):
+    if isinstance(symbol_or_const_on_pos, str):
+        function_on_pos = function_mapping[symbol_or_const_on_pos]
+    else:
+        function_on_pos = symbol_or_const_on_pos
+
+    if isinstance(function_on_pos, LangType):
         return function_on_pos
 
     input_positions = current_func.function_reference.input_mapping[pos]
@@ -234,11 +321,13 @@ def _execute_gen(
 
 
 def execute_entry(
-    current_func: FunctionFrame,
-    pos: Position,
+    current_func: Function,
+    pos: GeneralizedPosition,
     function_mapping: Dict[str, PossibleFunc],
-) -> Constant:
-    gen_obj: ExecuteGenReturnType = _execute_gen(current_func, pos, function_mapping)
+    args: List[LangType],
+) -> LangType:
+    func_frame: FunctionFrame = FunctionFrame(current_func, args)
+    gen_obj: ExecuteGenReturnType = _execute_gen(func_frame, pos, function_mapping)
     while True:
         try:
             trace = next(gen_obj)
@@ -250,6 +339,24 @@ def execute_entry(
 
 def main():
     print("Hello from toy-functional-language!")
+
+    function1: Function = Function(
+        symbol_mapping={(0, 0): "+"},
+        input_mapping={
+            ENTRY_POINT: [(0, 0)],
+            (0, 0): [ARG0, ARG1],
+        },
+        input_positions=[ARG0, ARG1],
+    )
+
+    function_mapping: Dict[str, PossibleFunc] = {
+        "+": OperationPlus(),
+        "f1": function1,
+    }
+
+    execute_entry(function1, ENTRY_POINT, function_mapping, [1, 2])
+
+    print("exit from toy-functional-language")
 
 
 if __name__ == "__main__":
